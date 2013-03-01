9aee09926c93ddb6a50025e2f100327016d2aa16
Marco van Hylckama Vlieg <marco@i-marco.nl>
2013-02-28 19:30:27 -0800

    Adding Spanish

---
+++ js/strings-es.js
@@ -0,0 +1,86 @@
+var weathercodes = {
+		"200" : "puta madre <strong>tormentas</strong> con un poco de lluvia",
+		"201" : "puta <strong>tormentas</strong> y est&aacute; meando duro",
+		"202" : "puta <strong>tormentas</strong> con lluvia apocal&iacute;ptica",
+		"210" : "quiere ser una <strong>tormenta</strong>",
+		"211" : "es una puta <strong>tormenta</strong>",
+		"212" : "mierda esta pesada esta <strong>tormenta</strong>",
+		"221" : "mierda que <strong>tormenta</strong> irregular",
+		"230" : "una puta <strong>tormenta</strong> con un chorrito peque&ntilde;o",
+		"231" : "una puta <strong>tormenta</strong> con un chorrito",
+		"232" : "una puta <strong>tormenta</strong> con un chorrito pesado",
+		"300" : "maldita <strong>llovizna</strong> ligera",
+		"301" : "maldita <strong>llovizna</strong>",
+		"302" : "maldita <strong>llovizna</strong> pesada",
+		"310" : "esta <strong>meando</strong> ligero",
+		"311" : "esta <strong>meando</strong>",
+		"312" : "esta <strong>meando</strong> pesado",
+		"321" : "<strong>Meando lluvia</strong>",
+		"500" : "esta <strong>meando</strong> ligerito",
+		"501" : "esta <strong>meando</strong> moderadamente",
+		"502" : "esta <strong>jodidamente lloviendo</strong> aurita",
+		"503" : "esta <strong>jodidamente lloviendo</strong>, estilo monz&oacute;n",
+		"504" : "hay un <strong>aguacero apocal&iacute;ptico</strong>. Preparen sus malditos barcos",
+		"511" : "puta madre hay congelaci&oacute;n en la <strong>lluvia</strong>",
+		"520" : "hay malditas <strong>lluvias</strong> ligeras",
+		"521" : "hay malditas <strong>lluvias</strong>",
+		"522" : "puta madre hay <strong>lluvias</strong> pesadas",
+		"600" : "hay puta <strong>nieve ligera</strong>",
+		"601" : "puta <strong>nieve</strong>",
+		"602" : "toneladas de puta <strong>nieve</strong>",
+		"622" : "toneladas de puta <strong>ducha de nieve</strong>",
+		"611" : "puta <strong>aguanieve</strong>",
+		"621" : "maldita desagradable <strong>ducha aguanieve</strong>",
+		"701" : "puta <strong>niebla</strong>",
+		"711" : "mierda <strong>humo</strong>",
+		"721" : "mierda <strong>neblina</strong>",
+		"731" : "puta madre <strong>arena/polvo gira</strong>",
+		"741" : "yay, puta <strong>niebla</strong>",
+		"800" : "mierda <strong>cielos</strong> claros",
+		"801" : "algunas putas <strong>nubes</strong>",
+		"802" : "disperso de putas <strong>nubes</strong>",
+		"803" : "mierda <strong>nubes</strong> rotas",
+		"804" : "mierda <strong>cubierto de nubles</strong>",
+		"900" : "un puto <strong>tornado</strong> ruge",
+		"901" : "una puta <strong>tormenta tropical</strong> se est&aacute; librando",
+		"902" : "puta madre un <strong>hurac&aacute;n</strong> se est&aacute; librando",
+		"903" : "mierda extremo el <strong>frio</strong>",
+		"904" : "mierda extremo el <strong>calor</strong>",
+		"905" : "esta <strong>ventoso</strong> como mierda",
+		"906" : "mierda <strong>granizo</strong>"
+	};
+
+	var temperatures = [];
+	temperatures[0] = "El infierno esta <strong>congelandose</strong>.";
+	temperatures[1] = "<strong>Frio</strong> como mierda.";
+	temperatures[2] = "Maldito <strong>frio</strong>.";
+	temperatures[3] = "Mas o menos.";
+	temperatures[4] = "Mierda que <strong>bonito</strong>.";
+	temperatures[5] = "Maldito <strong>calor</strong>.";
+	temperatures[6] = "<strong>Caliente</strong> que mierda.";
+
+var phrases = {
+    'fetching' : 'Obteniedo la informaci&oacute;n que ya tenemos. Pero carajo espera y se paciente, puede tomar un tiempo.',
+    'myweather' : 'Mi clima',
+    'near' : 'Estas serca de mierda',
+    'rightnow' : 'El puto clima aurita',
+    'outsidenight' : 'Yo pudriera mirar afuera pero <strong>mierda que oscuro</strong> que esta...',
+    'outside' : 'Pudriera mirar afuera para mas informacion...',
+    'attribution' : 'La Ultima App del Clima. By Marco van Hylckama Vlieg.',
+    'onlyon' : 'Solo en',
+    'yououtsidenight' : 'Pudrieras mirar afuera para mas informacion pero <strong>puta madre esta oscuro</strong>.',
+    'yououtside' : 'Puedes mirar afuera para mas informacion.',
+    'appname' : 'La <strong>Ultima</strong> App Del Clima',
+    'bymarco' : 'Por Marco van Hylckama Vlieg',
+    'copyright' : 'Autor &copy; 2013',
+    'idea' : 'Basado en una idea de Tobias van Schneider',
+    'poweredby' : 'Inpulsado por',
+    'screamager' : 'Te gusto? Intenta Screamager!',
+    'return' : 'Volver',
+    'setpersonal' : 'Establecer Mensaje del Clima Personal',
+    'invite' : 'Invita a bajar',
+    'sharemisery' : 'Comparte tu miseria',
+    'errorfetching' : 'Error a obtener el clima',
+    'lookoutsideafterall' : 'Parece que vas a tener que buscar fuera, despu&eacute;s de todo.',
+    'and' : 'y' // to link two weather descriptions, e.g. it's fucking raining and there's mist 
+};
